75a9091ebe4fc1df29e5015a4e1699e780145655
Yasmeen Mahilang <100535436+yaaassmeen@users.noreply.github.com>
2023-06-15 14:09:37 +0530

    Create 11. Intersection of 2  sorted Array.cpp
    https://leetcode.com/problems/intersection-of-two-arrays/

---
+++ 01Array-Easy/11. Intersection of 2  sorted Array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        set<int> st;
+        vector<int> ans;
+        int i=0, j = 0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else{
+                st.insert(nums1[i]);
+                i++;j++;
+            }
+        }
+        for(auto it : st){
+            ans.push_back(it);
+        }
+        return ans;
+        
+    }
+};
