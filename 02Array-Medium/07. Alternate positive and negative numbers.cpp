2d86b438f6f4d3e34d99adb776ab81245d7d6022
Yasmeen Mahilang <100535436+yaaassmeen@users.noreply.github.com>
2023-06-17 00:07:28 +0530

    Create 07. Alternate positive and negative numbers.cpp

---
+++ 02Array-Medium/07. Alternate positive and negative numbers.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int A[], int n) {
+	    // code here
+	vector<int> pos;
+  vector<int> neg;
+  for(int i=0;i<n;i++);
+      if(A[i]>=0) pos.push_back(A[i]);
+      else neg.push_back(A[i]);
+  }
+  if(pos.size() < neg.size()){
+ 
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+    }
+    
+    int index = pos.size()*2;
+    for(int i = pos.size();i<neg.size();i++){
+        
+        A[index] = neg[i];
+        index++;
+    }
+  }
+  else{
+      for(int i=0;i<neg.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+  }
+    int index = neg.size()*2;
+    for(int i = neg.size();i<pos.size();i++){
+        
+        A[index] = pos[i];
+        index++;
+    }
+  }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
