a2fd6a90151c22d647681aaf3a665c56c1a58aef
Yasmeen Mahilang <100535436+yaaassmeen@users.noreply.github.com>
2023-06-16 22:00:36 +0530

    Update 01. 2 Sum.cpp

--- 02Array-Medium/01. 2 Sum.cpp
+++ 02Array-Medium/01. 2 Sum.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> end;
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            int rem = target - nums[i];
+            if(mp.find(rem) != mp.end()){
+                end.push_back(min(i, mp[rem]));
+                end.push_back(max(i, mp[rem]));
+            }
+            mp[nums[i]] = i;
+        }
+        return end;
+    }
+};
