260d1f673763358eac05d53eee2a7528a6ae4c00
Yasmeen Mahilang <100535436+yaaassmeen@users.noreply.github.com>
2023-06-20 14:02:35 +0530

    Create 05.SubsetswithXORvalue.cpp

---
+++ 03Array-Hard/05.SubsetswithXORvalue.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int subsetXOR(vector<int> arr, int N, int K) {
+        // code here
+        int xr = 0;
+    map<int, int> mpp; //declaring the map.
+    mpp[xr]++; //setting the value of 0.
+    int cnt = 0;
+
+    for (int i = 0; i < N; i++) {
+        // prefix XOR till index i:
+        xr = xr ^ arr[i];
+
+        //By formula: x = xr^k:
+        int x = xr ^ K;
+
+        // add the occurrence of xr^k
+        // to the count:
+        cnt += mpp[x];
+
+        // Insert the prefix xor till index i
+        // into the map:
+        mpp[xr]++;
+    }
+    return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        int K;
+        cin>>K;
+        vector<int> v;
+        for(int i = 0;i<N;i++)
+        {
+            int x;
+            cin>>x;
+            v.push_back(x);
+        }    
+        Solution ob;
+        cout << ob.subsetXOR(v,N,K) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
