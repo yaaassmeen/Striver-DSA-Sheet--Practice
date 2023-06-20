67de1509ce16cb719d5a90b637ed0ec918e8a60b
Yasmeen Mahilang <100535436+yaaassmeen@users.noreply.github.com>
2023-06-20 18:18:10 +0530

    Create 07.InplaceMergeSort.cpp

---
+++ 03Array-Hard/07.InplaceMergeSort.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    void swapifGreater(long long arr1[], long long arr2[], int i, int j){
+        if(arr1[i] > arr2[j]){
+            swap(arr1[i], arr2[j]);
+        }
+    }
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            // code here 
+            int len = n+m;
+            int gap = len/2 + len%2;
+            while(gap>0){
+                int left = 0;
+                int right = left+gap;
+                while(right<len){
+                    //arr1 arr2
+                    if(left < n && right >= n){
+                        swapifGreater(arr1, arr2, left, right-n);
+                    }
+                    //arr2 arr2
+                    else if(left>=n){
+                        swapifGreater(arr2, arr2, left-n,right-n);
+                    }
+                    //arr1 arr1
+                    else{
+                        swapifGreater(arr1,arr1,left,right);
+                    }
+                    left++;
+                    right++;
+                }
+                if(gap == 1)break;
+                gap = gap/2 + gap%2;
+            }
+            
+        } 
+// 2nd method
+// void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         int i =m-1, j =0;
+//         while(i>=0 && j<n){
+//             if(nums1[i] > nums2[j]){
+//                 swap(nums1[i] , nums2[j]);
+//                 i--;
+//                 j++;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         sort(nums2.begin(),nums2.end());
+//         for(int i=m;i<n+m;i++){
+//             nums1[i] = nums2[i-m];
+//         }
+//         sort(nums1.begin(),nums1.end());
+//     }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+
+// } Driver Code Ends
