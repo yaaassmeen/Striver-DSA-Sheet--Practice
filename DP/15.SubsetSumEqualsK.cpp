f6e9f700da8e526e739859fa06e97c6f6660dbe1
Yasmeen Mahilang <100535436+yaaassmeen@users.noreply.github.com>
2023-08-18 22:58:29 +0530

    Update 15.SubsetSumEqualsK.cpp

--- DP/15.SubsetSumEqualsK.cpp
+++ DP/15.SubsetSumEqualsK.cpp
@@ -28,3 +28,28 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     return f(n-1, k, arr, dp);
 }
 //Tabulation
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    // Write your code here.
+    vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+    
+    for(int i=0; i<n; i++){
+        dp[i][0] = true;
+    }
+    if(arr[0]<=k)
+        dp[0][arr[0]] = true;
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 1; target<=k; target++){
+            
+            bool notTaken = dp[ind-1][target];
+    
+            bool taken = false;
+                if(arr[ind]<=target)
+                    taken = dp[ind-1][target-arr[ind]];
+        
+            dp[ind][target]= notTaken||taken;
+        }
+    }
+    
+    return dp[n-1][k];
+}
