822544cb8b76516ed04dedfa6367ee091ee655d2
Yasmeen Mahilang <100535436+yaaassmeen@users.noreply.github.com>
2023-08-28 19:18:11 +0530

    Create 27.LongestCommonSubstring.cpp

---
+++ DP/27.LongestCommonSubstring.cpp
@@ -0,0 +1,31 @@
+//Tabulation
+int lcs(string &str1, string &str2){
+    // Write your code here.
+    int n= str1.size(), m = str2.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    int maxi = 0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(str1[i-1] == str2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+            else dp[i][j] = 0;
+            maxi = max(dp[i][j], maxi);
+        }
+    }
+    return maxi;
+}
+//Space Optimization
+int lcs(string &str1, string &str2){
+    // Write your code here.
+    int n= str1.size(), m = str2.size();
+    vector<int> prev(m+1, 0), curr(m+1, 0);
+    int maxi = 0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(str1[i-1] == str2[j-1]) curr[j] = 1+prev[j-1];
+            else curr[j] = 0;
+            maxi = max(curr[j], maxi);
+        }
+        prev = curr;
+    }
+    return maxi;
+}
